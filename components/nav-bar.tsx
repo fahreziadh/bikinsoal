ab9c98939c786d38ac0131184d35603f1c1e2c05
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-14 06:21:43 +0700

    beta

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -5,8 +5,11 @@ import { Button } from './ui/button'
 const Navbar = () => {
   return (
     <div className='container flex justify-between py-8'>
+      <div className='inline-flex'>
         <Image alt='logo' src={"/logo.png"} width={175} height={100} />
-        <Button>Login</Button>
+        <div><span className='rounded-full bg-black px-4 py-1 text-sm text-white'>Beta</span></div>
+      </div>
+      <Button>Login</Button>
     </div>
   )
 }
