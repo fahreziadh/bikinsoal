204cf024066cf927eaad5f671ae09655d9b10599
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-14 23:50:39 +0700

    add prisma auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import prisma from "@/lib/prismadb"
+import { prisma } from "@/lib/db"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
