412894d765ac3fa4b58e225edf23f9835114af3a
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-14 12:35:22 +0700

    privacy page

---
+++ app/privacyandpolicy/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='container py-8'><p className='mb-4 text-xl font-bold'>Kebijakan Privasi:</p>
+
+    Jenis informasi yang kami kumpulkan: Kami dapat mengumpulkan informasi pribadi seperti nama, alamat email, dan informasi lain yang relevan untuk memberikan layanan kami. Kami juga dapat mengumpulkan informasi non-pribadi seperti informasi perambatan web dan penggunaan situs web.
+    
+    Bagaimana kami menggunakan informasi tersebut: Kami menggunakan informasi tersebut untuk memberikan layanan kami, mengelola akun Anda, memberikan informasi tentang produk dan layanan kami, serta meningkatkan kualitas layanan kami.
+    
+    Bagaimana kami melindungi informasi Anda: Kami menggunakan praktik keamanan yang sesuai untuk melindungi informasi Anda dari akses yang tidak sah, penggunaan, atau pengungkapan.
+    
+    Bagaimana kami membagikan informasi Anda: Kami tidak akan membagikan informasi pribadi Anda kepada pihak ketiga tanpa izin Anda kecuali diwajibkan oleh hukum atau perintah pengadilan.
+    
+    Bagaimana Anda dapat mengakses atau mengontrol informasi Anda: Anda dapat mengakses atau mengontrol informasi Anda dengan menghubungi kami melalui informasi kontak yang tertera di situs web kami.
+    
+    Kebijakan Situs Web:
+    
+    Tujuan situs web kami: Tujuan situs web kami adalah untuk menyediakan layanan yang berkualitas dan informasi yang bermanfaat kepada pengguna.
+    
+    Hak cipta dan penggunaan konten: Semua hak cipta dan hak kekayaan intelektual terkait konten di situs web kami adalah milik kami atau pihak ketiga yang memberikan izin kepada kami untuk menggunakan konten tersebut.
+    
+    Tautan ke situs web lain: Situs web kami mungkin memiliki tautan ke situs web lain yang tidak terkait dengan kami. Kami tidak bertanggung jawab atas konten atau praktik privasi situs web tersebut.
+    
+    Penggunaan cookie: Situs web kami dapat menggunakan cookie untuk memperbaiki pengalaman pengguna dan memberikan layanan yang lebih baik. Informasi lebih lanjut tentang penggunaan cookie kami dapat ditemukan di kebijakan privasi kami.
+    
+    Perubahan pada kebijakan situs web: Kami dapat memperbarui atau mengubah kebijakan situs web kami dari waktu ke waktu tanpa pemberitahuan sebelumnya. Pengguna kami diharapkan untuk mengunjungi halaman kebijakan situs web kami secara berkala untuk memastikan pemahaman terbaru tentang kebijakan tersebut.</div>
+  )
+}
+
+export default page
