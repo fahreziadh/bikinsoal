464791291edf4010b15b9296c6517b6509a8e008
Fahrezi <fahreziadh@gmail.com>
2023-03-20 08:46:26 +0700

    session client

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,22 @@
-import { authOptions } from '@/pages/api/auth/[...nextauth]'
-import { getServerSession } from 'next-auth'
+"use client"
+import { SessionProvider } from 'next-auth/react'
 import React from 'react'
 import SectionFeature from './home/SectionFeature'
 import SectionHeader from './home/SectionHeader'
 import SectionPricing from './home/SectionPricing'
 import SectionVideo from './home/SectionVideo'
 
-export const revalidate = 3600;
+const Page = () => {
 
-const Page = async() => {
-  const session = await getServerSession(authOptions)
-  
   return (
-    <div className='container'>
-      <SectionHeader session={session} />
-      <SectionVideo />
-      <SectionFeature />
-      <SectionPricing />
-    </div>
+    <SessionProvider>
+      <div className='container'>
+        <SectionHeader />
+        <SectionVideo />
+        <SectionFeature />
+        <SectionPricing />
+      </div>
+    </SessionProvider>
   )
 }
 
