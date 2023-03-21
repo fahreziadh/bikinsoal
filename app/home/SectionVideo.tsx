2615d6c565e838de47ba368749c69da3c0466b4a
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-21 23:16:44 +0700

    change cache

--- app/home/SectionVideo.tsx
+++ app/home/SectionVideo.tsx
@@ -4,7 +4,7 @@ const SectionVideo = () => {
   return (
     <div className="mt-20">
       <video controls={false} autoPlay muted loop playsInline className="shadow-lg">
-        <source src="https://da8idql80spie.cloudfront.net/demo1.mp4" type="video/mp4" />
+        <source src="/demo1.mp4" type="video/mp4" />
       </video>
     </div>
   );
