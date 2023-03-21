3e893833b379ec1b7d33432b43816e6f7bc28e09
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-22 00:18:47 +0700

    add bank soal feature

--- app/app/banksoal/page.tsx
+++ app/app/banksoal/page.tsx
@@ -6,14 +6,14 @@ import { Input } from '@/components/ui/input'
 import useDebounceValue from '@/hooks/use-debounce'
 import { fetcher } from '@/lib/fetcher'
 import { QuestionBank } from '@prisma/client'
-import Link from 'next/link'
 import React, { useState } from 'react'
+import { toast } from 'react-hot-toast'
 import useSWR from 'swr'
 
 const Page = () => {
   const [query, setQuery] = useState<string>('')
   const [page, setPage] = useState<number>(1)
-  const [limit, setLimit] = useState<number>(10)
+  const [limit] = useState<number>(20)
 
   const queryDebounce = useDebounceValue(query, 500)
 
@@ -21,17 +21,25 @@ const Page = () => {
 
 
   return (
-    <div className='flex min-h-screen w-full flex-col'>
+    <div className='flex min-h-screen w-full min-w-[1200px] flex-col'>
       <h1 className='mb-8 text-2xl font-bold'>List Soal</h1>
       <div className='mb-4 inline-flex w-full items-center gap-2'>
         <Input placeholder='Cari Soal' className='w-6/12' value={query} onChange={(e) => setQuery(e.target.value)} />
       </div>
       <div className='mb-2 flex flex-row items-center gap-4 rounded-md border p-3 text-sm text-zinc-600'>
-        <h2 className="mr-12 w-1/3 overflow-hidden truncate font-medium hover:text-clip">
+        <h2 className="mx-8 w-1/3 overflow-hidden truncate font-medium hover:text-clip">
           Pertanyaan
         </h2>
-        <span className='rounded-full px-4 py-1'>
+        <span className='w-2/12 rounded-full'>
           Jenis
+        </span>
+        
+        <span className='w-2/12 rounded-full'>
+          Mata Pelajaran
+        </span>
+        
+        <span className='w-2/12 rounded-full'>
+          Dibuat
         </span>
       </div>
 
@@ -40,7 +48,14 @@ const Page = () => {
 
       {data && data?.questions?.length > 0 && <div className="rounded-md border">
         {data?.questions?.map((item, index) => (
-          <Link key={item.id} href="/"><ItemQuestionBank index={index + 1} question={item} /></Link>
+          <div
+          className='cursor-pointer'
+           onClick={()=>toast('Coming Soon')}
+           key={item.id} >
+            <ItemQuestionBank
+            index={(page-1)*limit + (index + 1)} 
+            question={item} />
+          </div>
         ))}
       </div>
       }
--- components/question/item-question-bank.tsx
+++ components/question/item-question-bank.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import { QuestionBank } from '@prisma/client'
+import dayjs from 'dayjs'
 import { motion } from 'framer-motion'
 import React from 'react'
 
@@ -10,37 +11,29 @@ interface Props {
 
 const ItemQuestionBank = ({ question, index }: Props) => {
 
-    const itemVariants = {
-        hidden: { opacity: 0, y: 10 },
-        visible: { opacity: 1, y: 0 },
-    }
-
-    const itemTransition = {
-        type: 'spring',
-        duration: 0.3,
-        delay: index * 0.05, // tambahkan delay pada setiap item dengan nilai yang berbeda
-    }
-
 
     return (
         <motion.div
-            variants={itemVariants}
-            initial="hidden"
-            animate="visible"
-            transition={itemTransition}
-            className="flex flex-row items-center gap-4 border-b p-3 text-sm hover:bg-zinc-100">
+            className="flex flex-row items-center gap-4 border-b p-3 text-sm hover:bg-zinc-50">
             <span className="w-4 font-light text-zinc-600">{index}.</span>
-            <h2 className="mr-8 w-1/3 overflow-hidden truncate font-medium hover:text-clip"      >
+            <h2 className="mr-8 w-4/12 overflow-hidden truncate font-medium hover:text-clip"      >
                 {question.question}
             </h2>
-            <span
-                className={cn(
-                    'rounded-full px-4 py-1 text-xs font-bold ',
-                    question.a ? ' border border-indigo-500 bg-indigo-100 text-indigo-500' : 'border border-teal-500 bg-teal-100 text-teal-500'
-                )}
-            >
-                {question.a ? 'Pilihan Ganda' : 'Essay'}
-            </span>
+            <div className='w-2/12'>
+                <span
+                    className={cn(
+                        'rounded-full px-4 py-1 text-xs font-bold ',
+                        question.a ? ' border border-indigo-500 bg-indigo-100 text-indigo-500' : 'border border-teal-500 bg-teal-100 text-teal-500'
+                    )}>
+                    {question.a ? 'Pilihan Ganda' : 'Essay'}
+                </span>
+            </div>
+            <div className='w-2/12'>
+                <span>{question.subject || '-'}</span>
+            </div>
+            <div className='w-2/12 text-xs'>
+                <span>{dayjs(question.createdAt).format("HH:mm, D MMM YYYY")}</span>
+            </div>
         </motion.div>
     )
 }
