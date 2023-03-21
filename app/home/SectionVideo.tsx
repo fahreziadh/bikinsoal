055337b9039996be3cc28d0901ae4eea49b9d905
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-21 22:50:50 +0700

    remove video library

--- app/home/SectionVideo.tsx
+++ app/home/SectionVideo.tsx
@@ -1,23 +1,8 @@
 "use client"
-import React from 'react'
-import dynamic from 'next/dynamic'
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
 const SectionVideo = () => {
   return null;
-  return (
-    <ReactPlayer
-      autoPlay={true}
-      muted={true}
-      controls={false}
-      loop={true}
-      playing={true}
-      url='https://da8idql80spie.cloudfront.net/demo1.mp4'
-      width={"100%"}
-      height={"100%"}
-      style={{ objectFit: 'fill',aspectRatio: '16/9', marginTop: '80px', borderRadius: '10px', }}
-    />
-  )
+  // https://da8idql80spie.cloudfront.net/demo1.mp4
 }
 
 export default SectionVideo
