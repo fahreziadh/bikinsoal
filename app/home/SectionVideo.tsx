d032bd8fab6ed47b30bd6b995a2e2b841f8cac3e
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-21 23:11:28 +0700

    new video components

--- app/home/SectionVideo.tsx
+++ app/home/SectionVideo.tsx
@@ -1,8 +1,14 @@
 "use client"
 
 const SectionVideo = () => {
-  return null;
-  // https://da8idql80spie.cloudfront.net/demo1.mp4
+  return (
+    <div className="mt-20">
+      <video controls={false} autoPlay muted loop playsInline className="shadow-lg">
+        <source src="https://da8idql80spie.cloudfront.net/demo1.mp4" type="video/mp4" />
+      </video>
+    </div>
+  );
+  // 
 }
 
 export default SectionVideo
