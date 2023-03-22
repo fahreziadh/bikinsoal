ab314cc9e89b2610963db6d5e18cc93b6d8f842e
Fahrezi Adha <fahreziadh@gmail.com>
2023-03-22 21:22:30 +0700

    fix route api

--- app/api/payment/limit/route.ts
+++ app/api/payment/limit/route.ts
@@ -9,28 +9,52 @@ export const GET = async () => {
     if (!session?.user?.email) {
         return NextResponse.json({ message: 'Unauthorized' }, { status: 401 })
     }
-
     const total = await prisma.questionGenerated.aggregate({
         where: {
-            user: {
-                email: session?.user?.email
-            },
-            createdAt: {
-                gte: new Date(new Date().setHours(0, 0, 0, 0)),
-                lte: new Date(new Date().setHours(23, 59, 59, 999)),
-            },
+          user: {
+            email: session?.user?.email
+          },
+          createdAt: {
+            gte: new Date(new Date().setHours(0, 0, 0, 0)),
+            lte: new Date(new Date().setHours(23, 59, 59, 999)),
+          },
         },
         _sum: {
-            total: true,
+          total: true,
         },
-    })
-
-    const limit_max = 15
-
-
-    return NextResponse.json({
+      })
+    
+      let limit_max = 15
+      let have_subscription = false
+    
+      // get user's transaction depend on pack code and validate the date 
+      const transaction = await prisma.transaction.findMany({
+        where: {
+          user: {
+            email: session?.user?.email
+          },
+          is_active: true,
+          expired_at: {
+            gte: new Date()
+          }
+        },
+        select: {
+          package: true,
+          updatedAt: true,
+          is_active: true,
+          expired_at: true
+        }
+      })
+    
+      if (transaction && transaction.length > 0) {
+        have_subscription = true
+      }
+    
+      return {
         limit_left: limit_max - (total._sum.total || 0),
         limit_max,
+        expired_at: transaction.find((item) => item.is_active)?.expired_at?.toString(),
+        have_subscription: have_subscription,
         is_limit_reached: (total._sum.total || 0) >= limit_max,
-    })
+      }
 }
