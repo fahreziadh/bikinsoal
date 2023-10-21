ba75e06e894819f2e2ee9404ef5fb42f2cdd17ea
fahreziadh <fahreziadh@gmail.com>
2023-10-21 09:11:00 +0700

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button"
-import { cn } from "@/lib/utils"
 import "@/styles/globals.css"
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google"
 import Link from "next/link"
 
 export const metadata = {
-  title: 'BikinSoal - Generate Soal Online',
-  description: 'BikinSoal adalah platform untuk membuat soal secara online. Dengan fitur yang lengkap, membuat soal menjadi lebih mudah dan menyenangkan.',
+  title: "BikinSoal - Generate Soal Online",
+  description:
+    "BikinSoal adalah platform untuk membuat soal secara online. Dengan fitur yang lengkap, membuat soal menjadi lebih mudah dan menyenangkan.",
   keywords: [
     "AI",
     "ChatGPT",
@@ -30,22 +29,23 @@ export const metadata = {
     {
       name: "fahreziadh",
       url: "https://instagram.com/izerhaf_",
-    }
+    },
   ],
   creator: "fahreziadh",
   openGraph: {
-    type: 'website',
-    locale: 'id_ID',
-    url: 'https://bikinsoal.com/',
-    title: 'BikinSoal - Generate Soal Online',
-    description: 'BikinSoal adalah platform untuk membuat soal secara online. Dengan fitur yang lengkap, membuat soal menjadi lebih mudah dan menyenangkan.',
+    type: "website",
+    locale: "id_ID",
+    url: "https://bikinsoal.com/",
+    title: "BikinSoal - Generate Soal Online",
+    description:
+      "BikinSoal adalah platform untuk membuat soal secara online. Dengan fitur yang lengkap, membuat soal menjadi lebih mudah dan menyenangkan.",
   },
   twitter: {
     creator: "@fahreziadhaa",
-  }
+  },
 }
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default async function RootLayout({
   children,
@@ -54,15 +54,11 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
-      <body className={cn('dark:bg-[#0c0c0c] dark:text-white', inter.className)}>
-      <div className='flex min-h-screen w-full flex-col items-center justify-center'>
-      <h1 className="text-3xl font-bold">
-        Under Maintenance
-      </h1>
-      <Link href="https://revamp.bikinsoal.com/">
-         <Button className="mt-4 rounded-full bg-rose-600 hover:bg-rose-500">Lihat Website Revamp</Button>
-      </Link>
-    </div>
+      <body className={"dark:bg-[#0c0c0c] dark:text-white"}>
+        <div className="flex min-h-screen w-full flex-col items-center justify-center">
+          <h1 className="text-3xl font-bold">Under Maintenance</h1>
+          <Link href="https://revamp.bikinsoal.com/">Lihat Website Revamp</Link>
+        </div>
       </body>
     </html>
   )
