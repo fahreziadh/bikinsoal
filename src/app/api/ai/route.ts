3e3c877738e7c73eceee6684822779a9133346e7
fahreziadh <fahreziadh@gmail.com>
2023-10-22 10:30:32 +0700

    add rate limit on question generate

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -2,6 +2,8 @@
 import OpenAI from "openai";
 import { env } from "@/env.mjs";
 import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Ratelimit } from "@upstash/ratelimit";
+import { kv } from "@vercel/kv";
 
 const openai = new OpenAI({
   apiKey: env.OPENAI_API_KEY || "",
@@ -10,10 +12,29 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 export async function POST(req: Request) {
+  // Rate Limit
+
+  if (env.KV_REST_API_URL && env.KV_REST_API_TOKEN) {
+    const ip = req.headers.get("x-forwarded-for");
+    const ratelimit = new Ratelimit({
+      redis: kv,
+      // rate limit to 5 requests per 24 hours
+      limiter: Ratelimit.slidingWindow(5, "24h"),
+    });
+
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `ratelimit_${ip}`,
+    );
+
+    if (!success) {
+      return new Response("(e)Kamu Sudah mencapai batas request harian yaitu 5 kali, Coba Lagi Besok ya 🤪");
+    }
+  }
+
   const { prompt } = await req.json();
-  const params = new URL( req.url).searchParams
-  const withOption = params.get("withOption") === 'true' ? true : false
-  
+  const params = new URL(req.url).searchParams;
+  const withOption = params.get("withOption") === "true" ? true : false;
+
   const classificationResponse = await openai.completions.create({
     model: "gpt-3.5-turbo-instruct",
     stream: false,
@@ -31,20 +52,20 @@ export async function POST(req: Request) {
     total = 0,
     error = "",
   } = ClassifyInstruction(text ?? "{}");
-  
+
   if (error) {
     return new Response(error, { status: 400 });
   }
-  if (total > 15){
+  if (total > 15) {
     return new Response("(e)Jumlah soal maksimal 15");
   }
 
-  let generateQuizPrompt = ''
+  let generateQuizPrompt = "";
 
-  if(withOption){
+  if (withOption) {
     generateQuizPrompt = `Berikan soal pilihan ganda tanpa pilihan, hanya soal saja. berjumlah ${total} untuk ${grade} dengan mata pelajaran ${subject} dan topik ${topic}. pastikan hanya berikan soal dengan format berikut: (q)question(s)(q)question(s)(q)question(s). jangan ada urutan nomor pada awal soal. tanda (q) untuk question, dan (s) untuk stop.`;
-  }else{
-   generateQuizPrompt = `Berikan soal berjumlah ${total} untuk ${grade} dengan mata pelajaran ${subject} dan topik ${topic}. pastikan hanya berikan soal dengan format berikut: (q)question(s)(q)question(s)(q)question(s). jangan ada urutan nomor pada awal soal. tanda (q) untuk question, dan (s) untuk stop.`;
+  } else {
+    generateQuizPrompt = `Berikan soal berjumlah ${total} untuk ${grade} dengan mata pelajaran ${subject} dan topik ${topic}. pastikan hanya berikan soal dengan format berikut: (q)question(s)(q)question(s)(q)question(s). jangan ada urutan nomor pada awal soal. tanda (q) untuk question, dan (s) untuk stop.`;
   }
 
   const generateQuiz = await openai.completions.create({
