01dedbd2492977f3b9c5967a6e50addf046b4d1a
Fahrezi <fahreziadh@gmail.com>
2025-02-15 17:05:04 +0700

    Update project dependencies, styling, and landing page layout

---
+++ src/lib/components/ui/button/index.ts
@@ -0,0 +1,49 @@
+import { type VariantProps, tv } from "tailwind-variants";
+import type { Button as ButtonPrimitive } from "bits-ui";
+import Root from "./button.svelte";
+
+const buttonVariants = tv({
+	base: "ring-offset-background focus-visible:ring-ring inline-flex gap-2 items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 cursor-pointer active:scale-95 transition-all",
+	variants: {
+		variant: {
+			default: "bg-primary text-primary-foreground hover:bg-primary/90",
+			destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+			outline:
+				"border-input bg-background hover:bg-accent hover:text-accent-foreground border",
+			secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+			ghost: "hover:bg-accent hover:text-accent-foreground",
+			link: "text-primary underline-offset-4 hover:underline",
+		},
+		size: {
+			default: "h-9 px-4 py-2",
+			sm: "h-8 rounded-md px-3",
+			lg: "h-10 rounded-xl px-8",
+			icon: "h-9 w-9",
+		},
+	},
+	defaultVariants: {
+		variant: "default",
+		size: "default",
+	},
+});
+
+type Variant = VariantProps<typeof buttonVariants>["variant"];
+type Size = VariantProps<typeof buttonVariants>["size"];
+
+type Props = ButtonPrimitive.Props & {
+	variant?: Variant;
+	size?: Size;
+};
+
+type Events = ButtonPrimitive.Events;
+
+export {
+	Root,
+	type Props,
+	type Events,
+	//
+	Root as Button,
+	type Props as ButtonProps,
+	type Events as ButtonEvents,
+	buttonVariants,
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,33 @@
 <script>
-	import { Button } from "@/component/ui/button";
-
+	import { Button } from '@/components/ui/button';
+	import { Textarea } from '@/components/ui/textarea';
+	import { CommandIcon, CornerDownLeftIcon, LogInIcon, PlusIcon } from 'lucide-svelte';
 </script>
-<div class="container">
-  <Button>Click me</Button>
+
+<div class="flex min-h-screen flex-col">
+  <nav class="container py-4 flex flex-row justify-between">
+    <a class="font-medium text-2xl" href="/">Bikinsoal</a>
+    <div class="flex flex-row gap-2">
+      <Button variant="outline">
+        <span>Login</span>
+        <LogInIcon size={16} />
+      </Button>
+    </div>
+  </nav>
+	<div class="container max-w-2xl grow">
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt, cumque ad voluptatem
+		blanditiis laudantium et eaque ipsa sequi magnam sunt, sit modi at autem. Incidunt veniam amet
+		rem est quaerat?
+	</div>
+	<form class="relative mx-auto w-2xl mb-4">
+		<Textarea class="resize-none min-h-24" placeholder="Enter your text here"></Textarea>
+		<Button class="absolute bottom-2 right-2">
+			<span>Submit</span>
+			<div class="flex flex-row opacity-70">
+				<CommandIcon size={10} />
+				<PlusIcon size={10} />
+				<CornerDownLeftIcon size={10} />
+			</div>
+		</Button>
+	</form>
 </div>
